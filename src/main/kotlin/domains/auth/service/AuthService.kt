2175662fee2f6f60206380f67c892ea21a476ed1
bangddong <gusehdqkd@naver.com>
2025-10-07 00:28:54 +0900

    feat : Spring의 커스텀 AOP 사용시 Self Invocation이나 Pointcut path 작성 등 불편한 부분을 코틀린 방식으로 래핑하여 커스텀 Logging, Transactional 구현

--- src/main/kotlin/domains/auth/service/AuthService.kt
+++ src/main/kotlin/domains/auth/service/AuthService.kt
@@ -1,24 +1,35 @@
 package org.study.domains.auth.service
 
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 import org.study.common.exception.CustomException
 import org.study.common.exception.ErrorCode
 import org.study.common.jwt.JwtProvider
+import org.study.common.logging.Logging
+import org.study.common.transaction.Transactional
 import org.study.interfaces.OAuthServiceInterface
 
 @Service
 class AuthService (
     private val oAuth2Services: Map<String, OAuthServiceInterface>,
-    private val jwtProvider: JwtProvider
+    private val jwtProvider: JwtProvider,
+    private val logger: Logger = Logging.getLogger(AuthService::class.java),
+    private val transaction: Transactional
 ) {
-    fun handleAuth(state: String, code: String): String {
+
+    fun handleAuth(state: String, code: String): String = Logging.logFor(logger) { log ->
         val provider = state.lowercase()
+        log["provider"] = provider
 
         val callService = oAuth2Services[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
 
         val accessToken = callService.getToken(code)
         val userInfo = callService.getUserInfo(accessToken.accessToken)
         val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
+
+        transaction.run {
+
+        }
 
         return ""
     }
